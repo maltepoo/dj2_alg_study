4c8d990697a160a6b7d44aa1e4651fb0f872861b
jpNew-man <jaengpin@gmail.com>
2021-08-04 22:49:15 +0900

    solved

---
+++ b5585.py
@@ -0,0 +1,12 @@
+# n = 1000 - int(input())
+# cnt = 0
+
+# array = [500, 100, 50, 10, 5, 1]
+# for coin in array:
+#     cnt += n // coin
+#     n %= coin
+
+# print(cnt)
+
+
+n = 1000 - int(input())
