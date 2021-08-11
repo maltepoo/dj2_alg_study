e9531969132fe08b2aeb638d46b00e2e8f604c15
jpNew-man <jaengpin@gmail.com>
2021-08-11 11:00:28 +0900

    solved

---
+++ b13305.py
@@ -0,0 +1,11 @@
+C = int(input())
+R = list(map(int,input().split()))
+P = list(map(int,input().split()))
+
+mp = P[0] #처음 가격 최소로 설정
+res = 0
+for i in range(C-1): #마지막값 제외 순회
+    if mp > P[i]:
+        mp = P[i]
+    res += mp*R[i]
+print(res)
---
+++ b2872.py
@@ -0,0 +1,16 @@
+import sys
+
+T = int(input())
+books = [int(sys.stdin.readline())]
+cnt = 0
+
+for i in range(1, T):
+    books.append(int(sys.stdin.readline()))
+
+for j in range(T):
+    if books[-1-j] == T:
+        T -= 1
+    else:
+        cnt += 1
+print(cnt)
+
