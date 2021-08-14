e71916d27f501dbe6f29ad386d771d5452c188e1
jpNew-man <jaengpin@gmail.com>
2021-08-15 00:41:50 +0900

    solved

---
+++ W1_greedy/b16953.py
@@ -0,0 +1,17 @@
+A, B = list(map(int, input().split()))
+cnt = 1
+
+while B > A and B != 1:
+    if B % 10 == 1:
+        B = (B-1) // 10
+        cnt += 1
+    elif B % 2 == 0:
+        B = B // 2
+        cnt += 1
+    else:
+        cnt = -1
+        break
+if A == B:
+    print(cnt)
+else:
+    print(-1)
