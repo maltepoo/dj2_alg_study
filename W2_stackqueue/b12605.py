f61d6f6ef8e72222f4fb26df347b3231489078fb
jpNew-man <jaengpin@gmail.com>
2021-08-21 22:28:07 +0900

    solved

--- W2_stackqueue/b12605.py
+++ W2_stackqueue/b12605.py
@@ -1,5 +1,17 @@
 #12605. 단어순서 뒤집기
 N = int(input())
 for tc in range(1, N+1):
+    #일반풀이
     L = input().split()
     print('Case #{}: {}'.format(tc, " ".join(L[::-1])))
+
+    #스택풀이
+    L = input().split()
+    stack = []
+    ans = []
+    for l in L:
+        stack.append(l)
+    print("Case #{}:".format(tc), end=" ")
+    for _ in range(len(stack)):
+        print(stack.pop(), end=" ")
+    print()
