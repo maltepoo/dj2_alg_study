75e9bfeb411bc6b381e700ff474282b924f8d8e8
jpNew-man <jaengpin@gmail.com>
2021-10-06 01:22:47 +0900

    solved

--- W2_stackqueue/b1874.py
+++ W2_stackqueue/b1874.py
@@ -1,6 +1,32 @@
-import sys
-sys.stdin = open("input.txt")
 #1874. 스택 수열
-"""
-10/5 아침에 풀 것
-"""
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+ns = [int(input()) for _ in range(n)]
+
+i = 1
+idx = 0
+flag = True
+stack, res = [], []
+
+while idx < n:
+    if idx == n:
+        break
+    if stack and stack[-1] == ns[idx]:
+        stack.pop()
+        res.append('-')
+        idx += 1
+    elif i > ns[idx]:
+        flag = False
+        break
+    else:
+        stack.append(i)
+        res.append('+')
+        i += 1
+
+if flag:
+    for j in res:
+        print(j)
+else:
+    print('NO')
