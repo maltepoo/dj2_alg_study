21be4c653e84072dc7249e627c07a149557adb69
jpNew-man <jaengpin@gmail.com>
2021-11-07 18:57:18 +0900

    solved 11/7

---
+++ W2_stackqueue/b14713_앵무새.py
@@ -0,0 +1,25 @@
+#14713. 앵무새
+N = int(input())
+cnt, tcnt = 0, 0
+log = [0]*N
+word = []
+for _ in range(N):
+    temp = input().split()
+    cnt += len(temp)
+    word.append(temp)
+sent = input().split()
+while sent:
+    p = sent.pop(0)
+    tcnt += 1
+    for j in range(N):
+        flag = False
+        if log[j] <= len(word[j])-1 and p == word[j][log[j]]:
+            word[j][log[j]] = 0
+            log[j] += 1
+            flag = True
+            break
+        if j == (N-1) and flag == False:
+            print('Impossible')
+            exit(0)
+if sent or cnt != tcnt: print('Impossible')
+else: print('Possible')
