96200e51521703ec4e2e7ce0a3e68826c0c81986
jpNew-man <jaengpin@gmail.com>
2022-01-10 23:05:10 +0900

    solved: 1/10

--- programmers/level_1/비밀지도.py
+++ programmers/level_1/비밀지도.py
@@ -1,6 +1,5 @@
 def solution(n, arr1, arr2):
     answer = []
-    map1, map2 = arr1, arr2
     for i in range(n):
         answer.append(bin(map1[i] | map2[i])[2:].zfill(n).replace('1', '#').replace('0', ' '))
     return answer
