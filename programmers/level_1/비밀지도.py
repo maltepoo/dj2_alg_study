7ec2d35b25e11ec8109d567dadfc9ec3a04868ac
jpNew-man <jaengpin@gmail.com>
2022-01-10 23:08:33 +0900

    solved: 1/10

--- programmers/level_1/비밀지도.py
+++ programmers/level_1/비밀지도.py
@@ -1,5 +1,5 @@
 def solution(n, arr1, arr2):
     answer = []
     for i in range(n):
-        answer.append(bin(map1[i] | map2[i])[2:].zfill(n).replace('1', '#').replace('0', ' '))
+        answer.append(bin(arr1[i] | arr2[i])[2:].zfill(n).replace('1', '#').replace('0', ' '))
     return answer
